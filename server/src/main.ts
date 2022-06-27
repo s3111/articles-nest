49e19502cdaa32bcd5b55938063997259951344e
Serhii Vatulyev <4845637@gmail.com>
2022-06-27 18:55:03 +0300

    refactor

--- server/src/main.ts
+++ server/src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('/docs', app, document);
 
-  await app.listen(5000);
+  await app.listen(5001);
 }
 bootstrap()
   .catch((err) => {
